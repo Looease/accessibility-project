a7e7cf60560783cfb78bccfe70159efb22b6c64f
Miranda <miranda@something.com>
2019-11-30 13:27:32 +0000

    working in progress

--- script.js
+++ script.js
@@ -7,18 +7,30 @@ var ctx = canvas.getContext("2d");
 
 // all figures/objects will go in the state
 
-// var state = {
+var state = {
+  girl: {
+    walking: false;
+  }
+
 //   playerone:{
 //     x: 250,
 //     y: 250,
 //   },
 //
-// }
+}
 
 
-// This isn't working
-drawGirlWalk();
+setTimeout((){ state.girl.walking = true; }, 2000);
+// This isn't working as an animation - need help!
 
+function animateGirl() {
+  clearCanvas();
+  drawGirlStill();
+  clearCanvas();
+  drawGirlWalk();
+}
+
+setInterval(animateGirl,500);
 
 
 function drawGirlStill() {
@@ -40,6 +52,20 @@ function drawGirlWalk() {
 function clearCanvas() {
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+
+var coin1 = {
+  size: 20,
+  x: Math.floor(Math.random()*(3*canvas.width/10))+(canvas.width/10),
+  y: Math.floor(Math.random()*(3*canvas.height/10))+(canvas.height/10),
+  question1:"One of your online friends is",
+  question2: "going to help you with your",
+  question3: "homework, but has asked for",
+  question4: "your password.",
+  question5: "Should you give it to them?",
+  correctAnswer: state.buttons[1]
+
 }
 
 // I was thinking we could have a function that moves the player up and down past obstacles.
