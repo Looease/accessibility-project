764e6e2f04714314d2fcb66f7a1019101809cabd
akirakianna <kianna.akiralove@gmail.com>
2019-12-03 21:10:57 +0000

    Updates to files

---
+++ addUsername.js
@@ -0,0 +1,9 @@
+var urlParams = new URLSearchParams(window.location.search);
+
+console.log(urlParams.get('username'));
+console.log(urlParams.get('password'));
+
+
+// function URLSearchParams(){
+//
+// }
