1a750319b4f0e8569f418dcae418555b0ab51493
Miranda <miranda@something.com>
2019-12-05 07:00:15 +0000

    added text to alert

--- addUsername.js
+++ addUsername.js
@@ -1,7 +1,7 @@
 var urlParams = new URLSearchParams(window.location.search);
 
 console.log(urlParams.get('username'));
-console.log(urlParams.get('password'));
+// console.log(urlParams.get('password'));
 
 
 var input = (urlParams.get('username'));
@@ -12,7 +12,7 @@ var input = (urlParams.get('username'));
 
 
 window.onload = function alertMe() {
-  alert("Hello " + (urlParams.get('username')) + ", " + "let the game begin!");
+  alert("Hello " + (urlParams.get('username')) + ", " + "and welcome to the Dino Museum! Use the spacebar to start and stop the girl. Let the game begin!");
 
 }
 
